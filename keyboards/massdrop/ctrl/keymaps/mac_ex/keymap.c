1e465f8be040cc0af4b5af6a037e57caec0f6252
David Zech <david@davidzech.com>
2022-01-10 22:55:37 -0800

    Add capslock underglow indicator

--- keyboards/massdrop/ctrl/keymaps/mac_ex/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/mac_ex/keymap.c
@@ -26,9 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT(
         _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,            _______, _______, _______, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   KC_MPLY, KC_MSTP, KC_VOLU, \
-        _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, _______, _______, _______, _______,   KC_MPRV, KC_MNXT, KC_VOLD, \
-        _______, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______, _______, \
-        _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, _______, _______, _______, _______, _______,                              _______, \
+        _______, _______, _______, _______, _______, _______, _______, U_T_AUTO,U_T_AGCR,_______, _______, _______, _______, _______,   KC_MPRV, KC_MNXT, KC_VOLD, \
+        _______, _______,_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, RGB_TOG, _______, _______, _______, MD_BOOT, _______, _______, _______, _______, _______, _______,                              _______, \
         _______, _______, _______,                   _______,                            _______, _______, _______, _______,            _______, _______, _______ \
     ),
     /*
@@ -49,9 +49,18 @@ void matrix_init_user(void) {
     rgb_matrix_sethsv_noeeprom(HSV_COLOR_MODO2);
 };
 
+
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
 };
+
+void rgb_matrix_indicators_kb(void) {
+    if (host_keyboard_led_state().caps_lock) {
+        rgb_matrix_sethsv_noeeprom(0, 255, 200);
+    } else {
+        rgb_matrix_sethsv_noeeprom(HSV_COLOR_MODO2);
+    }
+}
 
 #define MODS_SHIFT  (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT))
 #define MODS_CTRL  (get_mods() & MOD_BIT(KC_LCTL) || get_mods() & MOD_BIT(KC_RCTRL))
@@ -109,27 +118,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             if (record->event.pressed) {
                 key_timer = timer_read32();
             } else {
-                if (timer_elapsed32(key_timer) >= 500) {
+                if (timer_elapsed32(key_timer) >= 2000) {
                     reset_keyboard();
                 }
             }
             return false;
-        case RGB_VAI:
-            if (record->event.pressed) {
-                rgb_matrix_increase_val_noeeprom();
-                if (rgb_matrix_get_val() > RGBLIGHT_LIMIT_VAL) {
-                    HSV hsv = rgb_matrix_get_hsv();
-                    hsv.v = RGBLIGHT_LIMIT_VAL;
-                    rgb_matrix_sethsv_noeeprom(hsv.h, hsv.s, hsv.v);
-                }
-            }
-            return false;
+        // case RGB_VAI:
+        //     if (record->event.pressed) {
+        //         rgb_matrix_increase_val_noeeprom();
+        //         if (rgb_matrix_get_val() > RGBLIGHT_LIMIT_VAL) {
+        //             HSV hsv = rgb_matrix_get_hsv();
+        //             hsv.v = RGBLIGHT_LIMIT_VAL;
+        //             rgb_matrix_sethsv_noeeprom(hsv.h, hsv.s, hsv.v);
+        //         }
+        //     }
+        //     return false;
         case RGB_TOG:
             if (record->event.pressed) {
               switch (rgb_matrix_get_flags()) {
                 case LED_FLAG_ALL: {
                     rgb_matrix_set_flags(LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER);
-                    rgb_matrix_set_color_all(0, 0, 0);
+                    rgb_matrix_set_color_all(0, 0, 0); // no eeprom write
                   }
                   break;
                 case LED_FLAG_KEYLIGHT | LED_FLAG_MODIFIER: {
